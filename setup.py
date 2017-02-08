2e034159b7fe1177e033216a28addd04dc8f5261
Eric Robertson <ericrobertson@Erics-MBP.home>
2017-02-08 09:53:15 -0500

    remove dsmoothdemo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='maskgen',
       data_files=[('resources',['resources/operations.json','resources/software.csv','resources/project_properties.json']),
                   ('plugins/Custom',
                    ['plugins/Custom/GammaCorrection.json', 'plugins/Custom/GaussianBlur.json', 'plugins/Custom/LevelCorrection.json',
-                    'plugins/Custom/Resize.json','plugins/Custom/SmoothDemo.json'
+                    'plugins/Custom/Resize.json'
                     ])],
       install_requires=['networkx','pillow','scikit-image','tkintertable','bitstring', 'boto', 'boto3','numpy','moviepy', 'h5py','pydot','graphviz','pygraphviz','rawpy'],
       entry_points=
