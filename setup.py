5875146831fe69739c7b4486b7acda6c6cdee912
Eric Robertson <ericrobertson@Erics-MBP.home>
2017-02-08 09:43:31 -0500

    SearchPath for resources and plugins global now

--- setup.py
+++ setup.py
@@ -8,12 +8,17 @@ setup(name='maskgen',
       author_email='eric_robertson@partech.com',
       license='APL',
       packages=find_packages(exclude=["images", "plugins", "resources", "tests"]),
+      data_files=[('resources',['resources/operations.json','resources/software.csv','resources/project_properties.json']),
+                  ('plugins/Custom',
+                   ['plugins/Custom/GammaCorrection.json', 'plugins/Custom/GaussianBlur.json', 'plugins/Custom/LevelCorrection.json',
+                    'plugins/Custom/Resize.json','plugins/Custom/SmoothDemo.json'
+                    ])],
       install_requires=['networkx','pillow','scikit-image','tkintertable','bitstring', 'boto', 'boto3','numpy','moviepy', 'h5py','pydot','graphviz','pygraphviz','rawpy'],
       entry_points=
        {'gui_scripts': [
             'jtui = maskgen.MaskGenUI:main',
         ],
-        'batch_scripts': [
+        'console_scripts': [
                'jtproject = maskgen.batch.batch_project:main',
                'jtprocess = maskgen.batch.batch_process:main'
            ]
