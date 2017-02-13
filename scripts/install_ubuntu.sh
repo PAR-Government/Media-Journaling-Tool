c4d6b8510d14502790c8f403ffa042ea6a1602de
Eric Robertson <ericrobertson@Erics-MacBook-Pro.local>
2017-02-13 11:36:48 -0500

    Ubuntu install script

---
+++ scripts/install_ubuntu.sh
@@ -0,0 +1,108 @@
+sudo apt-get install -y build-essential checkinstall make pkg-config
+sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+sudo apt-get install -y zlib1g-dev tk-dev
+sudo apt-get install -y git
+sudo apt-get install -y cmake mercurial
+sudo apt-get install -y libgtk2.0-dev
+sudo apt-get install -y pkg-config
+sudo apt-get install -y python python-numpy python-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev
+ 
+sudo apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev \
+  libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
+  libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev
+
+sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2 libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
+ 
+sudo apt-get -y install libx264-dev
+
+mkdir ~/ffmpeg_sources
+cd ~/ffmpeg_sources
+hg clone https://bitbucket.org/multicoreware/x265
+cd ~/ffmpeg_sources/x265/build/linux
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+make
+sudo make install
+make distclean
+
+sudo apt-get -y install zip
+sudo apt-get -y install libfdk-aac-dev
+sudo apt-get -y  install libmp3lame-dev
+sudo apt-get -y install libopus-dev
+
+cd ~/ffmpeg_sources
+wget http://storage.googleapis.com/downloads.webmproject.org/releases/webm/libvpx-1.6.0.tar.bz2
+tar xjvf libvpx-1.6.0.tar.bz2
+cd libvpx-1.6.0
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests
+PATH="$HOME/bin:$PATH" make
+sudo make install
+sudo make clean
+
+cd ~/ffmpeg_sources
+wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar xjvf ffmpeg-snapshot.tar.bz2
+cd ffmpeg
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+  --prefix="$HOME/ffmpeg_build" \
+  --pkg-config-flags="--static" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" \
+  --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --bindir="$HOME/bin" \
+  --enable-gpl \
+  --enable-libass \
+  --enable-libfdk-aac \
+  --enable-libfreetype \
+  --enable-libmp3lame \
+  --enable-libopus \
+  --enable-libtheora \
+  --enable-libvorbis \
+  --enable-libvpx \
+  --enable-libx264 \
+  --enable-libx265 \
+  --enable-nonfree
+PATH="$HOME/bin:$PATH" make
+sudo make install
+sudo make clean
+hash -r
+
+#cd ~
+#wget https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
+#tar xvf Python-2.7.13.tgz
+#cd Python-2.7.13
+#./configure --with-zlib --enable-shared
+#make
+#sudo make altinstall
+
+
+cd ~
+# download opencv-2.4.13
+wget http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.13/opencv-2.4.13.zip
+unzip opencv-2.4.13.zip
+cd opencv-2.4.13
+mkdir release
+cd release
+cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=/usr/bin/g++ CMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_FFMPEG=ON -DWITH_TBB=ON -DBUILD_NEW_PYTHON_SUPPORT=ON -DWITH_V4L=ON -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON -DBUILD_EXAMPLES=ON -DWITH_QT=ON -DWITH_OPENGL=ON -DBUILD_FAT_JAVA_LIB=ON -DINSTALL_TO_MANGLED_PATHS=ON -DINSTALL_CREATE_DISTRIB=ON -DINSTALL_TESTS=ON -DENABLE_FAST_MATH=ON -DWITH_IMAGEIO=ON -DBUILD_SHARED_LIBS=OFF -DWITH_GSTREAMER=ON ..
+make all -j2 # 2 cores
+sudo make install
+sudo /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+
+sudo apt-get install -y libimage-exiftool-perl
+sudo apt-get install -y libraw-dev
+sudo apt-get install -y graphviz graphviz-dev
+sudo apt-get install -y libhdf5-serial-dev
+sudo apt-get install -y libblas-dev liblapack-dev
+
+cd ~
+sudo pip install --upgrade pip
+sudo pip install rawpy
+sudo pip install graphviz
+sudo pip install numpy --upgrade
+sudo pip install PyPDF2
+
+https://github.com/rwgdrummer/maskgen.git
+pip install setuptools
+cd maskgen
+python setup.py install
