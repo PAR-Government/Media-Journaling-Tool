711768559701475a2cb27e6a205ec01b0bf4f42f
anddrrw <andrew_smith@partech.com>
2017-02-14 11:39:02 -0500

    Output Plugin Refactoring

--- plugins/OutputTIFF/__init__.py
+++ plugins/OutputTIFF/__init__.py
@@ -20,48 +20,46 @@ def tiff_save_as(source_img, source, target, donor_file, rotate):
     :param donor: string filename of donor TIFF
     :param rotate: boolean True if counter rotation is required
     """
-    donor_img = openImageFile(donor_file)
-    if rotate:
-        source_img = check_rotate(source_img, donor_img,donor_file)
-    source_img.save(target, format='TIFF', **donor_img.info)
-    width, height = source_img.size
-    maskgen.exif.runexif(['-overwrite_original', '-P', '-q', '-m', '-XMPToolkit=', target])
-    maskgen.exif.runexif(['-q', '-all=', target])
-    maskgen.exif.runexif(['-P', '-q', '-m', '-TagsFromFile', donor_file, '-all:all', '-unsafe', target])
-    maskgen.exif.runexif(['-P', '-q', '-m', '-XMPToolkit=',
-                 '-ExifImageWidth=' + str(width),
-                 '-ImageWidth=' + str(width),
-                 '-ExifImageHeight=' + str(height),
-                 '-ImageHeight=' + str(height),
-                 target])
+    if donor_file is not None:
+        donor_img = openImageFile(donor_file)
+        if rotate:
+            source_img = check_rotate(source_img, donor_img,donor_file)
+        source_img.save(target, format='TIFF', **donor_img.info)
+        maskgen.exif.runexif(['-overwrite_original', '-P', '-q', '-m', '-XMPToolkit=', target])
+        maskgen.exif.runexif(['-q', '-all=', target])
+        maskgen.exif.runexif(['-P', '-q', '-m', '-TagsFromFile', donor_file, '-all:all', '-unsafe', target])
+    else:
+        im = Image.open(source)
+        im.save(target, format='TIFF')
     createtime = maskgen.exif.getexif(target, args=['-args', '-System:FileCreateDate'], separator='=')
     if '-FileCreateDate' in createtime:
         maskgen.exif.runexif(['-P', '-q', '-m', '-System:fileModifyDate=' + createtime['-FileCreateDate'], target])
 
 
 def transform(source_img,source,target, **kwargs):
-    donor = kwargs['donor']
-    rotate = 'rotate' in kwargs and kwargs['rotate'] == 'yes'
+    donor = kwargs['donor'] if 'donor' in kwargs else None
+    rotate = 'Image Rotated' in kwargs and kwargs['Image Rotated'] == 'yes'
     tiff_save_as(source_img , source, target, donor, rotate)
     
     return None,None
     
 def operation():
-    return {'name':'AntiForensicExifQuantizationTable',
-            'category':'AntiForensic',
-            'description':'Save as a TIFF using original tables and EXIF',
+    return {'name':'OutputTif',
+            'category':'Output',
+            'description':'Save as a TIFF using original metadata, if donor provided',
             'software':'PIL',
             'version':'1.1.7',
             'arguments':{
                 'donor':{
                     'type':'donor',
                     'defaultvalue':None,
-                    'description':'TIFF with donor EXIF'
+                    'description':'TIFF with donor metadata'
                 },
-                'rotate':{'type':'yesno',
-                          'defaultvalue':None,
-                          'description':'Answer yes if the image should be counter rotated according to EXIF Orientation'
-                          }
+                'Image Rotated':{
+                    'type':'yesno',
+                    'defaultvalue':None,
+                    'description':'Answer yes if the image should be counter rotated according to EXIF Orientation'
+                    }
             },
             'transitions': [
                 'image.image'
