27c0908e9b18cfb9afb4aca8c2894fdc2942073b
Eric Robertson <ericrobertson@Erics-MBP.home>
2017-03-16 16:43:37 -0400

    Switch width/height of CR2 RawKit Opener

--- wrapper_plugins/rawphoto_wrapper/rawphoto_wrapper/opener.py
+++ wrapper_plugins/rawphoto_wrapper/rawphoto_wrapper/opener.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 def openRawFile(filename,isMask=None):
     from rawkit.raw import Raw
-    raw_image = Raw(filename)
-    buffered_image = np.array(raw_image.to_buffer())
-    return (np.array(Image.frombytes('RGB', (raw_image.metadata.width, raw_image.metadata.height), buffered_image)),'RGB')
+    with Raw(filename=filename) as raw_image:
+        buffered_image = np.array(raw_image.to_buffer())
+        return (np.array(Image.frombytes('RGB',
+                                         (raw_image.metadata.height, raw_image.metadata.width), buffered_image)),'RGB')
