b691bfa7a3339c41dc3b1b4241e1f76c7cb9a3e9
Xu Zhang <spongezhang@gmail.com>
2017-09-10 09:19:23 -0400

    Using bounding box center to calculate the offsets and splice, providing correct transform

--- plugins/PasteSplice/__init__.py
+++ plugins/PasteSplice/__init__.py
@@ -11,6 +11,7 @@ import numpy as np
 import math
 from skimage.restoration import denoise_tv_bregman
 from maskgen import cv2api
+#import maskgen.tool_set
 
 from sys import platform as sys_pf
 if sys_pf == 'darwin':
@@ -37,9 +38,9 @@ class TransformedEllipse(Ellipse):
             .scale(self.fix_x, 1) \
             .translate(*center)
 
-
 def minimum_bounding_box(image):
-    (contours, _) = cv2api.findContours(image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    #(contours, _) = cv2.findContours(image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2api.findContours(image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     selected = []
     for cnt in contours:
         try:
@@ -47,14 +48,19 @@ def minimum_bounding_box(image):
             x = int(M['m10'] / M['m00'])
             y = int(M['m01'] / M['m00'])
             x1, y1, w, h = cv2.boundingRect(cnt)
+            #To align with the width and height. Bounding box info is always 1 pixel larger than that.
+            w = w-1
+            h = h-1
             selected.append((w,h,w*h,x,y))
         except:
             continue
+        
     selected = sorted(selected, key=lambda cnt: cnt[2], reverse=True)
+    
     if len(selected) == 0:
-        print 'cannot determine contours'
         x, y, w, h = tool_set.widthandheight(image)
         selected = [ (w,h,w*h,x+w/2,y+h/2)]
+    
     return selected[0]
 
 def minimum_bounding_ellipse_of_points(points):
@@ -123,19 +129,26 @@ def transform_image(image,transform_matrix):
     """
     return cv2.warpAffine(image,transform_matrix,(image.shape[1],image.shape[0]))
 
-
 def build_random_transform(img_to_paste, mask_of_image_to_paste, image_center):
-    scale = 0.5 +  random.random()
+    scale = 0.5 + random.random()
     angle = 180.0*random.random() - 90.0
     return cv2.getRotationMatrix2D(image_center, angle, scale)
 
 def pasteAnywhere(img, img_to_paste, mask_of_image_to_paste, simple):
-    w, h, area, x, y = minimum_bounding_box(mask_of_image_to_paste)
+    #get gravity center for rotation
+    w, h, area, cx_gra, cy_gra = minimum_bounding_box(mask_of_image_to_paste)
+    #bounding box center for simple case.
+    x, y, w1, h1 = tool_set.widthandheight(mask_of_image_to_paste)
+
     if not simple:
-        rot_mat = build_random_transform(img_to_paste,mask_of_image_to_paste,(x,y))
+        #use gravity center to rotate
+        rot_mat = build_random_transform(img_to_paste,mask_of_image_to_paste,(cx_gra,cy_gra))
         img_to_paste = cv2.warpAffine(img_to_paste, rot_mat, (img_to_paste.shape[1], img_to_paste.shape[0]))
         mask_of_image_to_paste= cv2.warpAffine(mask_of_image_to_paste, rot_mat, (img_to_paste.shape[1], img_to_paste.shape[0]))
-        w, h, area, x, y = minimum_bounding_box(mask_of_image_to_paste)
+        #x,y is the Geometry center(gravity center), which can't align to the crop center(bounding box center)
+        w, h, area, cx, cy = minimum_bounding_box(mask_of_image_to_paste)
+        #So we use this line to calculate the bbox centor
+        x, y, w1, h1 = tool_set.widthandheight(mask_of_image_to_paste)
     else:
         rot_mat = np.array([[1,0,0],[0,1,0]]).astype('float')
 
@@ -144,7 +157,6 @@ def pasteAnywhere(img, img_to_paste, mask_of_image_to_paste, simple):
         xplacement = w / 2 + 1
     else:
         xplacement = random.randint(w / 2 + 1, img.size[0] - w / 2 - 1)
-
 
     if img.size[1] < h + 4:
         h = img.size[1] - 2
@@ -157,14 +169,17 @@ def pasteAnywhere(img, img_to_paste, mask_of_image_to_paste, simple):
     for i in range(2):
         for j in range(2):
             output_matrix[i, j] = rot_mat[i, j]
-            output_matrix[0, 2] = rot_mat[0, 2] + xplacement - x
-            output_matrix[1, 2] = rot_mat[1, 2] + yplacement - y
+    
+    #That is the correct offset
+    output_matrix[0,2] = rot_mat[0,2] + xplacement - x - w1/2
+    output_matrix[1,2] = rot_mat[1,2] + yplacement - y - h1/2
 
     return output_matrix, tool_set.place_in_image(
                           ImageWrapper(img_to_paste).to_mask().to_array(),
                           img_to_paste,
                           np.asarray(img),
                           (xplacement, yplacement),
+                        #x,y have no use
                           rect=(x, y, w, h))
 
 def transform(img,source,target,**kwargs):
@@ -191,13 +206,14 @@ def transform(img,source,target,**kwargs):
                     labels1 = segmentation.slic(gray, compactness=5, n_segments=min(500, int(imgsize/(sigma*2*masksize))))
                 else:
                     labels1= segmentation.felzenszwalb(gray, scale=min_size, sigma=sigma, min_size=int(min_size))
-
+                
                 #Compute the Region Adjacency Graph using mean colors.
                 #
                 # Given an image and its initial segmentation, this method constructs the corresponding  RAG.
                 # Each node represents a set of pixels  within image with the same label in labels.
                 # The weight between two adjacent regions represents how similar or dissimilar two
                 # regions are depending on the mode parameter.
+                
                 cutThresh = 0.000000005
                 labelset = np.unique(labels1)
                 while len(labels1) > 100000 or len(labelset) > 500:
@@ -242,8 +258,10 @@ def transform(img,source,target,**kwargs):
                     if out2 is not None:
                         break
                 sigma+=0.5
+    
     if out2 is None:
-        transform_matrix, out2 = pasteAnywhere(img, img_to_paste.to_array(),mask_of_image_to_paste, approach=='simple')
+        transform_matrix, out2 = pasteAnywhere(img, img_to_paste.to_array(), mask_of_image_to_paste, approach=='simple')
+        
     ImageWrapper(out2).save(target)
     return {'transform matrix':tool_set.serializeMatrix(transform_matrix)} if transform_matrix is not None else None,None
 
@@ -277,7 +295,7 @@ def operation():
           'transitions': [
               'image.image'
           ]
-  }
+}
 
 def suffix():
     return None
