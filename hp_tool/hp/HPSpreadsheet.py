c93df1741cdfedd1ee682fea8a7a629cd45fb493
justaduck <justin_firsching@partech.com>
2018-03-23 14:21:21 -0400

    Fixed incorrect exts reference

--- hp_tool/hp/HPSpreadsheet.py
+++ hp_tool/hp/HPSpreadsheet.py
@@ -210,18 +210,18 @@ class HPSpreadsheet(Toplevel):
         :param event: event trigger
         :return: None
         """
-        from hp.hp_data import exts
+        import hp.hp_data
         if self.on_main_tab:
             row = self.pt.getSelectedRow()
         else:
             row = self.tabpt.getSelectedRow()
         type = self.pt.model.getValueAt(row, 32)
         current_file = str(self.pt.model.getValueAt(row, 0))
-        self.imName = str(self.pt.model.getValueAt(row, 0) if (type == "image" and "." + self.pt.model.getValueAt(row, 13) not in exts['nonstandard']) else self.pt.model.getValueAt(row, 61).split(";")[0])
+        self.imName = str(self.pt.model.getValueAt(row, 0) if (type == "image" and "." + self.pt.model.getValueAt(row, 13) not in hp_data.exts['nonstandard']) else self.pt.model.getValueAt(row, 61).split(";")[0])
         self.currentImageNameVar.set('Current Image: ' + current_file)
         maxSize = 480
         try:
-            if type == "image" and "." + self.pt.model.getValueAt(row, 13) not in exts['nonstandard']:
+            if type == "image" and "." + self.pt.model.getValueAt(row, 13) not in hp_data.exts['nonstandard']:
                 im = Image.open(os.path.join(self.imageDir, self.imName))
             elif type == "model":
                 im = Image.open(os.path.join(self.modelDir, current_file.split(".")[0], self.imName))
