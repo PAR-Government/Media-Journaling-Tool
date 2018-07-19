784b321c7f77a21f16a41dc401f183450880aecc
hurlbuttm <michael_hurlbutt@partech.com>
2018-07-19 13:08:31 -0400

    Watcher Format and checking

--- maskgen/export/watcher.py
+++ maskgen/export/watcher.py
@@ -11,17 +11,27 @@ class ExportWatcherDialouge(Toplevel):
         self.parent = parent
         Toplevel.__init__(self, parent)
         self.exdir = os.path.join(os.path.expanduser('~'),'ExportLogs')
+        self.files = []
         self.createWidgets()
 
     def createWidgets(self):
-        mainFrame = Frame(self)
+        self.mainFrame = Frame(self)
+        self.header = Frame(self.mainFrame)
+        self.headerlbl = Label(self.header, text='File Name \t\t Progress/Status \t')
+        self.headerlbl.pack()
+        self.header.pack()
         if not os.path.isdir(self.exdir):
             os.mkdir(self.exdir)
-        for f in os.listdir(self.exdir):
-            ep = ExportProgress(mainFrame,os.path.join(self.exdir,f))
-            ep.pack()
-        mainFrame.pack()
+        self.checkDir()
+        self.mainFrame.pack()
+        self.after(1000,self.checkDir)
 
+    def checkDir(self):
+        for f in os.listdir(self.exdir):
+            if f not in self.files:
+                self.files.append(f)
+                ep = ExportProgress(self.mainFrame,os.path.join(self.exdir,f))
+                ep.pack()
 
 class ExportProgress(Frame):
     def __init__(self, parent, tfile):
@@ -38,7 +48,11 @@ class ExportProgress(Frame):
         self.prjlbl.grid(row=0,column=0)
         self.pb.grid(row=0,column=1)
         self.percentlbl.grid(row=0,column=2)
-        Button(self, text='STOP', command=self.stop, width=20).grid(column=3, row=0, sticky=E, padx=5, pady=5)
+        self.stop = Button(self, text='STOP', command=self.stop, width=10)
+        self.stop.grid(column=3, row=0, sticky=E, padx=5, pady=5)
+        self.stopped = False
+        self.removebtn = Button(self, text='Remove', command=self.remove, width=10)
+        self.removebtn.grid(column=4, row=0, sticky=E, padx=5, pady=5)
         self.update()
 
     def getdata(self):
@@ -54,6 +68,8 @@ class ExportProgress(Frame):
             percent = data[-1][start:end]
             if percent.lower() == 'do':
                 percent = 100, 'Complete'
+            elif percent.lower() == 'fa':
+                percent = 0, 'Failed'
             else:
                 percent = float(percent),'Working {}%'.format(percent)
             return percent
@@ -63,7 +79,7 @@ class ExportProgress(Frame):
         return self.pid
 
     def update(self):
-        # This causes control-C event
+        # This causes control-C event but was a good thought
         # try:
         #     os.kill(self.pid,0)
         # except WindowsError:
@@ -75,18 +91,37 @@ class ExportProgress(Frame):
         #     else:
         #         self.setpb(99.999999)
         #     return
+
         status, text = self.getdata()
-        self.setpb(status)
-        self.percentlbltxt.set(text)
-        self.percentlbl.after(1000, self.update, )
+        if text == 'Complete':
+            self.percentlbltxt.set('Complete')
+            self.setpb(99.999999)
+            self.stop.configure(state=DISABLED)
+            #return
+        elif text == 'Failed':
+            self.stop.configure(state=DISABLED)
+            self.percentlbltxt.set('Failed')
+        else:
+            self.stop.configure(state=ACTIVE)
+            self.setpb(status)
+            self.percentlbltxt.set(text)
+        if not self.stopped:
+            self.percentlbl.after(1000, self.update, )
         return
 
+
+    def remove(self):
+        if self.percentlbltxt.get() in ['Failed','Complete','STOPPED']:
+            os.remove(self.file)
+            self.grid_forget()
+            self.destroy()
     def setpb(self,x):
         self.pb['value']=x
-
     def stop(self):
         try:
-            return
             os.kill(self.pid, 7)
+            self.stop.configure(state=DISABLED)
+            self.percentlbltxt.set('STOPPED')
+            self.stopped = True
         except:
             tkMessageBox.showerror("Stop Failed", "Failed to stop export process possible lack of permissions")
