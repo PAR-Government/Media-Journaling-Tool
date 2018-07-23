9596199f8e953bb65ac9828a1d76103744b700a8
justaduck <justin_firsching@partech.com>
2018-07-23 09:50:08 -0400

    Edit export message
    Change popup message and relocate to after compression

--- maskgen/MaskGenUI.py
+++ maskgen/MaskGenUI.py
@@ -420,9 +420,9 @@ class MakeGenUI(Frame):
                                        initialvalue=info if info is not None else '')
         if (val is not None and len(val) > 0):
             self.prefLoader.save('s3info', val)
-            tkMessageBox.showinfo("Starting", "Export beginning the JT will now close please open another journal to work on and then open "
-                                              "the Export Manager to veiw progress.")
-            self.scModel.exporttos3(val, additional_message=message,external=True)
+            self.scModel.exporttos3(val, additional_message=message, external=True)
+            tkMessageBox.showinfo("Starting", "View the Export Manager to check progress.  Any changes to the current "
+                                              "journal will not be uploaded.")
             uploaded = self.prefLoader.get_key('lastlogupload')
             uploaded = exportlogsto3(val, uploaded)
             self.prefLoader.save('s3info', val)
