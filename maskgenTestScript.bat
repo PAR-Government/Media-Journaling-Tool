76bb1cfdb5d0adb43e25eb6b3cbe35d601796aaa
mag8224 <mag8224@g.rit.edu>
2018-07-31 12:45:10 -0400

    Fixing proper pathing

--- maskgenTestScript.bat
+++ maskgenTestScript.bat
@@ -6,9 +6,6 @@ cd ..
 cd batch
 python -m unittest discover
 cd ..
-cd external
-python -m unittest discover
-cd ..
 cd ioc
 python -m unittest discover
 cd ..
@@ -17,9 +14,9 @@ cd CocoMaskSelector
 python -m unittest discover
 cd ..
 cd ..
-cd plugins
-python -m unittest discover -s tests/plugins -p "*Test.py" -v 
 cd ..
+python -m unittest discover -s tests/plugins -p "*Test.py" -v
+cd tests
 cd segmentation
 python -m segmanagetest
 cd ..
