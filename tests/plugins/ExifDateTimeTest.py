e34767c3bef76c62154ca4d0f35a0d36cb2a0507
justaduck <justin_firsching@partech.com>
2018-10-03 10:33:26 -0400

    ExifDateTime Plugin (#693)
    * Added Plugin to change date and time of exifdata for a video
    
    Added plugin to change date and/or time of a donor video given a minimum and a maximum to generate a random point between the two.
    
    * Used more efficient date and time randomization
    
    Used datetime conversions to randomize date more efficiently
    
    * Video process json change and made ffmpeg run command more efficient
    
    Adjusted the json file for video_process and changed the runffmpeg command to overwrite an already created video rather than generating yet another duplicate
    
    * Add Unit Test, Fix Bug
    
    Added unit tests for random time and random date generation
    
    Fixed inability to generate times between PM and AM. (ex. 10:00PM - 3:00AM)
    
    * Remove Second Version

---
+++ tests/plugins/ExifDateTimeTest.py
@@ -0,0 +1,34 @@
+import unittest
+from plugins import ExifDateTime
+from datetime import datetime
+
+
+class ExifDateTimeTests(unittest.TestCase):
+    def test_random_date(self):
+        date = ExifDateTime.random_date("01-01-2010", "12-31-2020")
+        assert(date is not False)
+        assert(datetime.strptime("01-01-2010", "%m-%d-%Y").date() <=
+               datetime.strptime(date, "%Y-%m-%d").date() <=
+               datetime.strptime("12-31-2020", "%m-%d-%Y").date())
+
+        date = ExifDateTime.random_date("12-31-2020", "01-01-2010")
+        assert(date is False)
+
+        date = ExifDateTime.random_date("12-31-2020", "12-31-2020")
+        assert(date is not False)
+        assert(datetime.strptime(date, "%Y-%m-%d").date() == datetime.strptime("12-31-2020", "%m-%d-%Y").date())
+
+    def test_random_time(self):
+        time = ExifDateTime.random_time("23:00:00", "03:00:00")
+        assert(time is not False)
+
+        time = ExifDateTime.random_time("08:00:00", "12:00:00")
+        assert(time is not False)
+        assert(datetime.strptime("08:00:00", "%H:%M:%S").time() <=
+               datetime.strptime(time, "%H:%M:%S").time() <=
+               datetime.strptime("12:00:00", "%H:%M:%S").time())
+
+        time = ExifDateTime.random_time("15:00:00", "15:00:00")
+        assert(time is not False)
+        assert(datetime.strptime("15:00:00", "%H:%M:%S").time() ==
+               datetime.strptime(time, "%H:%M:%S").time())
