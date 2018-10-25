6c910c14e151448e600c1d7f6b618a5130705b43
mag8224 <32079393+mag8224@users.noreply.github.com>
2018-10-25 18:37:20 -0400

    Semantic group changes (#714)
    * Adding Falsifying Documents in Semantic Groups
    
    * Added Image for Falsifying Documents
    
    * Added Semantic Group Test
    
    * Slides added
    
    * Added basic Powerpoint
    
    * Updating Powerpoint and images

--- tests/test_operations_help.py
+++ tests/test_operations_help.py
@@ -55,6 +55,60 @@ class TestOperationsHelp(TestSupport):
         self.assertTrue(len(data["operation"]) == 0, "There are extra operation(s) in the help section")
 
         self.remove_files()
+
+    def test_semantic_group_slides(self):
+        downloadLink = "https://s3.amazonaws.com/medifor/browser/journal/SemanticGroups.pptx"
+        powerpointPlace = self.locateFile("resources/semanticGroups.pptx")
+
+        r = requests.get(downloadLink)
+        with open(powerpointPlace, 'wb+') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+        fd.close()
+
+        imgLinker = self.locateFile("resources\help\image_linker.json")
+        groups = self.locateFile("resources\project_properties.json")
+
+        prs = Presentation("../resources/SemanticGroups.pptx")
+        prs.save('Operations2.pptx')
+        self.addFileToRemove('Operations2.pptx')
+
+        slide = len(prs.slides)
+        print 'Number of slides gotten from online: ' + str(slide)
+
+        jtLocation = '../resources/help/semanticSlides'
+        path, dirs, slides = next(os.walk(jtLocation))
+        print "JT Semantic Slides: " + str(len(slides))
+        self.assertTrue(slide == len(slides), "Number of Slides online doesn't match number of slides in the JT")
+
+
+        with open(imgLinker) as f:
+            data = json.load(f)
+        with open(groups) as f2:
+            semGroups = json.load(f2)
+        semanticGroups = []
+        for d in semGroups["properties"]:
+            try:
+                if d["semanticgroup"]:
+                    semanticGroups.append(d)
+            except:
+                pass
+
+        images = set()
+        missing = []
+        for d in semanticGroups:
+            g = d["description"]
+            if g not in data["semanticgroup"] or len(data["semanticgroup"][g]["images"]) ==0 or data["semanticgroup"][g]["images"][0] =="":
+                missing.append(g)
+            else:
+                for i in data["semanticgroup"][g]["images"]:
+                    images.add(i)
+                data["semanticgroup"].pop(g)
+
+        self.assertTrue(missing == [], "Missing is not empty " + str(missing))
+        self.assertTrue(len(data["semanticgroup"]) == 0, "There are extra operation(s) in the help section")
+
+        self.remove_files()
         
 
 if __name__ == '__main__':
