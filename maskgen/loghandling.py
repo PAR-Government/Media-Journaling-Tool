ab62daf797f0ade7829bf724759046cfe3a45654
Eric Robertson <eric_robertson@partech.com>
2019-05-24 14:04:40 +0000

    Export manager crash
    Former-commit-id: 76be2b8093dfc4be2820630dc5be82e50b787778

--- maskgen/loghandling.py
+++ maskgen/loghandling.py
@@ -56,7 +56,7 @@ def set_logging(directory=None, filename='maskgen.log',skip_config=False,logger_
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
     # create formatter
-    formatter = logging.Formatter('%(asctime)s - %(name)s[%(threadName)s]- %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(name)s[%(threadName)s:%(process)d]- %(levelname)s - %(message)s')
 
     # add formatter to ch
     ch.setFormatter(formatter)
